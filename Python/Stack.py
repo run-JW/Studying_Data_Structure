7a7f36e6ba1a92151df3aa8dadd661cbb187e710
run-JW <65688438+run-JW@users.noreply.github.com>
2021-02-04 13:26:14 +0900

    Update Stack.py

--- Python/Stack.py
+++ Python/Stack.py
@@ -1,19 +1,19 @@
 class Stack():
-    def __init__(self):
+    def __init__(self): # Constructor
         self.items = []
         
-    def push(self, item):
+    def push(self, item): # Create
         self.items.append(item)
         
-    def pop(self):
+    def pop(self): # Delete
         return self.items.pop()
 
-    def is_empty(self):
+    def is_empty(self): # Check the list it is empty
         return self.items == []
 
-    def peek(self):
+    def peek(self): # if stack isn't empty then print the lastest element.
         if not self.is_empty():
             return self.items[-1]
     
-    def get_stack(self):
+    def get_stack(self): # Print list of elements
         return self.items
