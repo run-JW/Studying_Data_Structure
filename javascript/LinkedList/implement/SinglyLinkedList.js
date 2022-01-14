0ce02fd4ab46549fbb5e509861810f217f0d4990
run-JW <65688438+run-JW@users.noreply.github.com>
2022-01-14 10:00:00 +0900

    Create SinglyLinkedList.js
    to be continue

---
+++ javascript/LinkedList/implement/SinglyLinkedList.js
@@ -0,0 +1,36 @@
+class Structure {
+    search(list, key) { throw new Error("Abstract method"); }
+
+    insert(list, element) { throw new Error("Abstract method"); }
+}
+
+class Node {
+    constructor(key) {
+        this.key = key;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList extends Structure {
+    constructor() {
+        super();
+        this.count = 0;
+        this.head = null;
+        this.tail = null; 
+    }
+
+    get size() {
+        return this.count;
+    }
+
+    insert(value) {
+        if (this.head === null) {
+            this.head = new Node(value);
+            this.tail = this.head;
+        } else {
+            this.tail.next = new Node(value);
+            this.tail = this.tail.next;
+        }
+        this.count++;
+    }
+}
